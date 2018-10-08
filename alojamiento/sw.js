ab14ff5a4e7c5c1883cea2cb445ae94807d70f77
vicgom892 <vicgom89@gmail.com>
2018-10-08 20:32:26 -0300

    cambio

---
+++ alojamiento/sw.js
@@ -0,0 +1,10 @@
+Fetch: Network first (server or connection errors)
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      fetch(event.request).catch(function(error) {
+        return caches.open("mi-PWA").then(function(cache) {
+          return cache.match(event.request);
+        });
+      })
+    );
+  });
