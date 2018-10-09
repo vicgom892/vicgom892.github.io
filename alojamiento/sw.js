df1bb253fadfe9f578070f7943d501d5659c0c38
vicgom892 <vicgom89@gmail.com>
2018-10-08 22:05:50 -0300

    ss

--- alojamiento/sw.js
+++ alojamiento/sw.js
@@ -1,10 +1,25 @@
-//Fetch: Network first (server or connection errors)
+var urls = ["/", "/img/"]
+self.addEventListener("install", event => {
+    event.waitUntil(caches.open("mi-PWA")
+        .then( cache => cache.addAll(urls) ));
+});
+
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      fetch(event.request).catch(function(error) {
-        return caches.open("mi-PWA").then(function(cache) {
-          return cache.match(event.request);
-        });
-      })
-    );
-  });
+  event.respondWith(
+      caches.match(event.request)
+          .then(function(response) {
+              // Even if the response is in the cache, we fetch it
+              // and update the cache for future usage
+              var fetchPromise = fetch(event.request).then(
+                  function(networkResponse) {
+                      caches.open("mi-PWA").then(function(cache) {
+                          cache.put(event.request, networkResponse.clone());
+                          return networkResponse;
+                      });
+                  });
+              // We use the currently cached version if it's there
+              return response || fetchPromise;
+          })
+      );
+  }); 
+
