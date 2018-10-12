3dc8a0c10f7c3a4021ea1209bdaf68e633663192
vicgom892 <vicgom89@gmail.com>
2018-10-11 22:42:49 -0300

    sw cambio

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener('fetch', function(){
-    console.log('Escuchando un fetch')
-})
+// self.addEventListener('fetch', function(){
+//     console.log('Escuchando un fetch')
+// })
+
+var urls = ['/', '/script.js', '/style.cs'];
+
+self.addEventListener("install", function(event) {
+    console.log("The SW is now installed"); 
+    event.waitUntil(caches.open("myAppCache").then(function(cache) {
+        return cache.addAll(urls);
+    }));
+});
+
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      fetch(event.request).catch(function(error) {
+        return caches.open("mi-PWA").then(function(cache) {
+          return cache.match(event.request);
+        });
+      })
+    );
+  });
