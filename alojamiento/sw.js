d1e8bc0852e4cbb714a31bbbc5f16da1cecedbef
vicgom892 <vicgom89@gmail.com>
2018-10-11 22:23:46 -0300

    cambio icono

--- alojamiento/sw.js
+++ alojamiento/sw.js
@@ -1,25 +1,25 @@
-var urls = ["/", "/img/"]
-self.addEventListener("install", event => {
-    event.waitUntil(caches.open("mi-PWA")
-        .then( cache => cache.addAll(urls) ));
-});
+// var urls = ["/", "/img/"]
+// self.addEventListener("install", event => {
+//     event.waitUntil(caches.open("mi-PWA")
+//         .then( cache => cache.addAll(urls) ));
+// });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-      caches.match(event.request)
-          .then(function(response) {
-              // Even if the response is in the cache, we fetch it
-              // and update the cache for future usage
-              var fetchPromise = fetch(event.request).then(
-                  function(networkResponse) {
-                      caches.open("mi-PWA").then(function(cache) {
-                          cache.put(event.request, networkResponse.clone());
-                          return networkResponse;
-                      });
-                  });
-              // We use the currently cached version if it's there
-              return response || fetchPromise;
-          })
-      );
-  }); 
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//       caches.match(event.request)
+//           .then(function(response) {
+//               // Even if the response is in the cache, we fetch it
+//               // and update the cache for future usage
+//               var fetchPromise = fetch(event.request).then(
+//                   function(networkResponse) {
+//                       caches.open("mi-PWA").then(function(cache) {
+//                           cache.put(event.request, networkResponse.clone());
+//                           return networkResponse;
+//                       });
+//                   });
+//               // We use the currently cached version if it's there
+//               return response || fetchPromise;
+//           })
+//       );
+//   }); 
 
