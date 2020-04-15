c307485e470e24ddfd8514b2fc312adf1aff46eb
victor gomez <vicgom892@gmail.com>
2020-04-15 00:15:02 -0300

    agregar app

---
+++ app-beiro/js/app.js
@@ -0,0 +1,18 @@
+if (navigator.serviceWorker){
+    navigator.serviceWorker.register('/SW.js')
+             .then( reg => {
+
+
+
+                // Notification.requestPermission().then(result =>{
+
+                //     console.log('acepto');
+                //     reg.showNotification('hola mundo');
+    
+                //  });
+
+             });
+
+
+            
+}
