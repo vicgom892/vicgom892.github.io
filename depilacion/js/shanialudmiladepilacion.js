233cda00c513edf74cd235b712642d339fd32a51
victor gomez <vicgom892@gmail.com>
2020-04-14 23:49:58 -0300

    nuevos cambios

--- depilacion/js/shanialudmiladepilacion.js
+++
@@ -1,6 +0,0 @@
-var btnmenu=document.getElementById('btn-menu');
-var nav=document.getElementById('nav');
-
-btnmenu.addEventListener('click', function(){
-    nav.classList.toggle('muestrate');
-});
