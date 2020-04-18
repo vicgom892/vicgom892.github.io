619ae976ab1d3e0829719132693485ff12d1dbdd
victor gomez <vicgom892@gmail.com>
2020-04-18 01:02:46 -0300

    modificando

--- app-beiro/js/titulo.js
+++ app-beiro/js/titulo.js
@@ -1,7 +1,18 @@
-var text = document.getElementById('text')
-var shadow = '';
-for(var i = 0; i < 10; i++)
-{
-    shadow +=(shadow? ',':'')+ -i*1+'px '+ i*1+'px 0 #0423316e';
+// var text = document.getElementById('text')
+// var shadow = '';
+// for(var i = 0; i < 10; i++)
+// {
+//     shadow +=(shadow? ',':'')+ -i*1+'px '+ i*1+'px 0 #0423316e';
+// }
+// text.style.textShadow = shadow;
+var text = document.getElementById('text');
+var word = text.getElementsByTagName('span');
+
+var i = 0;
+
+function rotator(){
+    word[i].style.display = 'none';
+    i = (i + 1) % word.length;
+    word[i].style.display = 'initial';
 }
-text.style.textShadow = shadow;
+setInterval(rotator, 800);
