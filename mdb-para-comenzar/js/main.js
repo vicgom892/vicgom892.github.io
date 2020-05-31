de82de930eb60f195a9c2146165b1e7bdc264c53
victor gomez <vicgom892@gmail.com>
2020-05-30 23:25:40 -0300

    ok

--- mdb-para-comenzar/js/main.js
+++ mdb-para-comenzar/js/main.js
@@ -4,3 +4,21 @@
 //     .then(registration => console.log('Service worker registrado'))
 //     .catch(err => 'SW registration failed'));
 // }
+
+    //   if('serviceWorker' in navigator){
+    //     navigator.serviceWorker.register('/sw.js')
+    //     .then(function(){
+    //         console.log('Service Worker Registrado !!!')
+    //     }).catch(function(){
+    //         console.log('No se registro SW')
+    //     })
+    // }
+    if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("/sw.js")
+        .then(function(registration) {
+    // Worker is registered   
+    }).catch(function(error) {
+    // There was an error registering the SW 
+    });     
+}
+  
